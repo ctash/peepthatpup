b76ff380db6fc0a7d13f25ec842de059e4736e62
Devon Gundry <devon@chain.com>
2018-01-05 09:45:23 +1100

    client side resizing

--- scripts.js
+++ scripts.js
@@ -17,7 +17,34 @@ function readURL(input) {
             img.src = e.target.result;
 
             img.onload = function() {
-              rawdata = { dog_image: e.target.result.split(',')[1], image_size: [this.height, this.width] };
+              var canvas = document.createElement("canvas");
+              var ctx = canvas.getContext("2d");
+              ctx.drawImage(img, 0, 0);
+
+              var MAX_WIDTH = 800;
+              var MAX_HEIGHT = 600;
+              var width = img.width;
+              var height = img.height;
+
+              if (width > height) {
+                if (width > MAX_WIDTH) {
+                  height *= MAX_WIDTH / width;
+                  width = MAX_WIDTH;
+                }
+              } else {
+                if (height > MAX_HEIGHT) {
+                  width *= MAX_HEIGHT / height;
+                  height = MAX_HEIGHT;
+                }
+              }
+              canvas.width = width;
+              canvas.height = height;
+              var ctx = canvas.getContext("2d");
+              ctx.drawImage(img, 0, 0, width, height);
+
+              var dataurl = canvas.toDataURL("image/jpeg");
+
+              rawdata = { dog_image: dataurl.split(',')[1], image_size: [canvas.height, canvas.width] };
               console.log(rawdata)
 
               jsondata= JSON.stringify(rawdata)
